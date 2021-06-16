30247c75f0b3630bde53750d0cfa6b629b864dea
dajun1230 <1123395547@qq.com>
2021-06-16 21:49:02 +0800

    chore: Initialize project configuration

---
+++ vue.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  outputDir: "dist",
+  assetsDir: "",
+  indexPath: "index.html",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "Vue study",
+      chunks: ["chunk-vendors", "chunk-common", "index"],
+    },
+  },
+  lintOnSave: true,
+  devServer: {
+    hot: true,
+    clientLogLevel: "warning",
+    host: "localhost",
+    port: 8000,
+    https: false,
+    hotOnly: false,
+    open: true,
+    // proxy: {
+    //   "/api": {
+    //     target: "<url>",
+    //     ws: true,
+    //     changeOrigin: true,
+    //   },
+    // },
+  },
+  productionSourceMap: false,
+  // css: {
+  //   extract: true,
+  //   sourceMap: false,
+  //   loaderOptions: {
+  //     sass: {
+  //       prependData: `@import "~@/assets/css/variable.scss";`,
+  //     },
+  //   },
+  //   modules: false,
+  // },
+  pluginOptions: {},
+};
