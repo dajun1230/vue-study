803380dbf3b9c6260e0e970716ef7ec6143e72f6
dajun1230 <1123395547@qq.com>
2021-08-06 07:46:23 +0800

    feat: MVVM initialization

---
+++ src/mvvm/01-defineProperty.js
@@ -0,0 +1,47 @@
+// 对象响应式原理
+// 1. Object.defineProperty()
+
+function defineReactive(obj, key, val) {
+  // val可能是对象，需要递归处理
+  observe(val);
+
+  Object.defineProperty(obj, key, {
+    get() {
+      console.log("get", val);
+      return val;
+    },
+    set(newVal) {
+      if (newVal !== val) {
+        console.log("set", newVal);
+        observe(newVal);
+        val = newVal;
+      }
+    },
+  });
+}
+
+// 对象响应式处理
+function observe(obj) {
+  // 判断obj类型必须是对象
+  if (typeof obj !== "object" || obj === null) {
+    return;
+  }
+  Object.keys(obj).forEach((key) => defineReactive(obj, key, obj[key]));
+}
+
+function set(obj, key, val) {
+  defineReactive(obj, key, val);
+}
+
+const obj = { foo: "foo", bar: "bar", baz: { a: 1 } };
+// defineReactive(obj, "foo", "foo1");
+observe(obj);
+// obj.foo;
+// obj.foo = "foo2";
+// obj.bar;
+// obj.baz.a = 10;
+// obj.baz = { a: 10 }; // 新的值为对象
+//obj.baz.a = 100;
+// obj.dong = "dong"; // 对于新的属性处理
+set(obj, "dong", "dong1");
+obj.dong;
